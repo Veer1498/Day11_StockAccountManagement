ebdc7f579ea961cfe0cc17f5e3ad095b1783a461
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-07 22:59:29 +0530

    Welcome Message

---
+++ src/oopsproblems/StockAccount.java
@@ -0,0 +1,11 @@
+package oopsproblems;
+
+public class StockAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Stock Account Management");
+
+	}
+
+}
